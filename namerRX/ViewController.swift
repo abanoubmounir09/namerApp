d953fc5bb1ee0e8c4f2ff8708bfd1c6fcaec68c7
pop <pop@pops-mac.local>
2020-04-15 02:40:33 -0700

    Initial Commit

---
+++ namerRX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  namerRX
+//
+//  Created by pop on 4/15/20.
+//  Copyright © 2020 pop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
